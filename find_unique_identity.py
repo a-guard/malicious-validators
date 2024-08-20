2585efe0c0fc84f2ee5f7d7c31b22a22786ccdd9
Nika Efimova <nikikot@list.ru>
2024-08-21 02:05:14 +0300

    moderated

--- find_unique_identity.py
+++ find_unique_identity.py
@@ -6,4 +6,4 @@ with open('data/slot-leader.json', 'r') as f: d = orjson.loads(f.read())
 schindler_list = set()
 for (slot, sig) in d.items(): schindler_list.add(sig)
 
-with open('data/schindler_list.txt', 'w') as f: f.writelines('\n'.join(list(schindler_list)))
+with open('data/unique_identity_list.txt', 'w') as f: f.writelines('\n'.join(list(schindler_list)))
