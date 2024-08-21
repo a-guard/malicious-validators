2225d010fba725709a42aac910bd766f8ebe5531
Nika Efimova <nikikot@list.ru>
2024-08-21 04:27:41 +0300

    add jito delegation list

--- find_by_stake_authority.py
+++ find_by_stake_authority.py
@@ -5,7 +5,7 @@ import time
 
 # https://docs.stakewiz.com/reference/api-reference/validators
 STAKE_WIZ_API: str = 'https://api.stakewiz.com/'
-SFDP_STAKE_AUTH: str = 'mpa4abUkjQoAvPzREkh5Mo75hZhPFQ2FSH6w7dWKuQ5'
+STAKE_AUTH: str = '6iQKfEyhr3bZMotVkW6beNZz5CPAkiwvgV2CTje9pVSS'
 
 
 def check_stake_by_vote(vote_acc: str, stake_auth: str) -> float:
@@ -32,7 +32,6 @@ def count_sandwitches(identity_acc: str):
 
 
 with open('data/slot-leader.json', 'r') as f: d = orjson.loads(f.read())
-
 with open('data/filtered_sandwitches.json', 'r') as f: filtered_sandwitches = orjson.loads(f.read())
     
 identity_l = set()
@@ -48,15 +47,15 @@ sandwitches_total = 0
 for v in js:
     if v['identity'] in identity_l:
         stake = v['activated_stake']
-        delegated = check_stake_by_vote(v['vote_identity'], SFDP_STAKE_AUTH)
-        time.sleep(0.1) # to prevent rate limiting
-        if delegated == 0: continue
+        delegated = check_stake_by_vote(v['vote_identity'], STAKE_AUTH)
+        # time.sleep(0.1) # to prevent rate limiting
+        if delegated < 1: continue
         sandwitches_n = count_sandwitches(v['identity'])
         sandwitches_total += sandwitches_n
         activated_stake += v['activated_stake']
         delegated_by_stake += delegated
         print(v['identity'], v['vote_identity'], '{:_}'.format(int(delegated)), sandwitches_n)
 
-print('total sandwitches:', sandwitches_total)
+print('total sandwiches:', sandwitches_total)
 print('total: {:_} SOL'.format(int(activated_stake)))
-print('delegated by SFDP: {:_} SOL'.format(int(delegated_by_stake)))
+print('delegated by {}: {:_} SOL'.format(STAKE_AUTH, int(delegated_by_stake)))
